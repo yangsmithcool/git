f915797aeda97f67f165fc93de1ead1fe420ac31
Brandon Williams <bmwill@google.com>
2018-01-02 16:18:25 -0800

    transport-helper: introduce connect-half-duplex
    Introduce the transport-helper capability 'connect-half-duplex'.  This
    capability indicates that the transport-helper can be requested to run
    the 'connect-half-duplex' command which should attempt to make a
    half-duplex connection with a remote end.  Once established, the
    half-duplex connection can be used by the git client to communicate with
    the remote end natively in a stateles-rpc manner as supported by
    protocol v2.
    
    If a half-duplex connection cannot be established then the remote-helper
    will respond in the same manner as the 'connect' command indicating that
    the client should fallback to using the dumb remote-helper commands.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport-helper.c
+++ transport-helper.c
@@ -26,6 +26,7 @@ struct helper_data {
 		option : 1,
 		push : 1,
 		connect : 1,
+		connect_half_duplex : 1,
 		signed_tags : 1,
 		check_connectivity : 1,
 		no_disconnect_req : 1,
@@ -188,6 +189,8 @@ static struct child_process *get_helper(struct transport *transport)
 			refspecs[refspec_nr++] = xstrdup(arg);
 		} else if (!strcmp(capname, "connect")) {
 			data->connect = 1;
+		} else if (!strcmp(capname, "connect-half-duplex")) {
+			data->connect_half_duplex = 1;
 		} else if (!strcmp(capname, "signed-tags")) {
 			data->signed_tags = 1;
 		} else if (skip_prefix(capname, "export-marks ", &arg)) {
@@ -612,6 +615,11 @@ static int process_connect_service(struct transport *transport,
 	if (data->connect) {
 		strbuf_addf(&cmdbuf, "connect %s\n", name);
 		ret = run_connect(transport, &cmdbuf);
+	} else if (data->connect_half_duplex) {
+		strbuf_addf(&cmdbuf, "connect-half-duplex %s\n", name);
+		ret = run_connect(transport, &cmdbuf);
+		if (ret)
+			transport->stateless_rpc = 1;
 	}
 
 	strbuf_release(&cmdbuf);
